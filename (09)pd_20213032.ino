850b38200b9fd5f6de5bff77c39ec1fad642cd6f
hongbuly <60245930+hongbuly@users.noreply.github.com>
2021-12-13 12:46:08 +0900

    Update (09)pd_20213032.ino

--- (09)pd_20213032.ino
+++ (09)pd_20213032.ino
@@ -10,9 +10,9 @@
 #define _DIST_MIN 70
 #define _DIST_MAX 410
 
-#define _DUTY_MIN 1600 //up
-#define _DUTY_NEU 1700
-#define _DUTY_MAX 1980 //down
+#define _DUTY_MIN 1330 //up
+#define _DUTY_NEU 1450
+#define _DUTY_MAX 1650 //down
 
 #define _SERVO_ANGLE 30
 #define _SERVO_SPEED 30 // servo speed limit (unit: degree/second)
@@ -21,12 +21,12 @@
 // Event periods
 #define _INTERVAL_DIST 20
 #define _INTERVAL_SERVO 20
-#define _INTERVAL_SERIAL 100
+#define _INTERVAL_SERIAL 500
 
 // PID parameters
-#define _KP 0.75;
-#define _KI 0.0007;
-#define _KD 32;
+#define _KP 0.62;
+#define _KI 0.0014;
+#define _KD 16.7;
 
 Servo myservo;
 
@@ -49,6 +49,11 @@ float kp, ki, kd;
 float dist_min, dist_max, dist_cali, dist_prev, alpha;
 int a, b; // unit: mm
 
+float median_q[5];
+float arr[5];
+int now = 0;
+float median_pivot = 0.0;
+
 void setup() {
   // initialize GPIO pins
   pinMode(PIN_LED, OUTPUT);
@@ -56,19 +61,22 @@ void setup() {
 
   // move servo to neutral position
   myservo.attach(PIN_SERVO);
-  myservo.writeMicroseconds(_DUTY_MIN);
+  myservo.writeMicroseconds(_DUTY_NEU);
 
   // initialize serial port
   Serial.begin(57600);
 
   // initialize global varibles
-  a = 76; //real 100
-  b = 399; //real 400
+  a = 70; //real 100
+  b = 393; //real 400
   dist_target = _DIST_TARGET;
   dist_min = _DIST_MIN;
   dist_max = _DIST_MAX;
   dist_ema = dist_raw = dist_cali = dist_prev = 0.0;
   alpha = _DIST_ALPHA;
+
+  for (int i = 0; i < 5; i++)
+    median_q[i] = arr[i] = 0.0;
 
   // convert angle speed into duty change per interval
   duty_chg_per_interval = (_DUTY_MAX - _DUTY_MIN) * (_SERVO_SPEED / 180.0) * (INTERVAL / 1000.0);
@@ -91,11 +99,43 @@ float ir_distance(void) { // return value unit: mm
   float volt = float(analogRead(PIN_IR));
   val = ((6762.0 / (volt - 9.0)) - 4.0) * 10.0;
   if (val < dist_min || val > dist_max) val = 0.0;
+  if (dist_prev != 0)
+    if (dist_prev - val > 50 && dist_prev - val < 150)
+      val = 0.0;
 
   if (val == 0.0) val = dist_prev;
   else dist_prev = val;
 
   return val;
+}
+
+void swap(float arr[], int a, int b) {
+  float temp = arr[a];
+  arr[a] = arr[b];
+  arr[b] = temp;
+}
+
+void partition(float *median_q, int start, int ends) {
+  for (int i = 0; i < 5; i++) {
+    arr[i] = median_q[i];
+  }
+  int pivot = start;
+  int i = pivot + 1;
+  int j = ends;
+
+  while (i <= j)
+  {
+    while (i <= ends && arr[i] <= arr[pivot]) ++i;
+    while (j > start && arr[j] >= arr[pivot]) --j;
+
+    if (i >=  j) {
+      break;
+    }
+
+    swap(arr, i, j);
+  }
+  swap(arr, j, pivot);
+  median_pivot = arr[2];
 }
 
 void loop() {
@@ -120,11 +160,20 @@ void loop() {
     // get a distance reading from ir_distance
     dist_raw = ir_distance();
     dist_cali = 100 + 300.0 / (b - a) * (dist_raw - a);
-    dist_ema = alpha * dist_cali + (1 - alpha) * dist_ema;
+    if (now > 4) {
+      now--;
+      for (int i = 1; i < 5; i++) {
+        median_q[i - 1] = median_q[i];
+      }
+    }
+    median_q[now++] = dist_cali;
+    partition(median_q, 0, 4);
+
+    dist_ema = alpha * median_pivot + (1 - alpha) * dist_ema;
 
     // PID control logic
     error_curr = dist_target - dist_ema;
-    pterm = error_curr;
+    pterm = kp * error_curr;
 
     if (-8 < error_curr && error_curr < 8)
     {
@@ -132,20 +181,26 @@ void loop() {
     }
     else if (-45 < error_curr && error_curr < 45)
     {
-      iterm += error_curr * _INTERVAL_DIST;
+      iterm +=  ki * error_curr;
     }
     else {
       iterm = 0;
     }
-    dterm = (error_curr - error_prev) / _INTERVAL_DIST;
+    dterm = kd * (error_curr - error_prev);
 
-    //contrl = kp * pterm + ki * iterm + kd * dterm;
-    contrl = kp * pterm + kd * dterm;
+    // control smoothly pd
+    //    if (pterm < 0)
+    //      pterm *= -((int)pterm / 90.0);
+    //    else
+    //      pterm *= ((int)pterm / 70.0);
+    if (dterm > 45 || dterm < -40) dterm /= (dterm / 3);
+    if ((pterm < 0 && dterm < 0) || (pterm > 0 && dterm > 0)) dterm *= -1;
+
+    contrl = pterm + dterm;
     duty_target = _DUTY_NEU + contrl;
     duty_curr = alpha * duty_target + (1 - alpha) * duty_target;
 
     //keep control value within the range
-    //contrl = map(contrl, -180, 180, _DUTY_MIN, _DUTY_MAX);
     if (duty_target < _DUTY_MIN) duty_target = _DUTY_MIN;
     if (duty_target > _DUTY_MAX) duty_target = _DUTY_MAX;
 
@@ -155,22 +210,28 @@ void loop() {
   if (event_servo) {
     event_servo = false;
     // adjust servo position according to the ir_distance
-    myservo.writeMicroseconds(duty_target);
+    myservo.writeMicroseconds(duty_curr);
   }
 
   if (event_serial) {
     event_serial = false;
     // output the read value to the serial port
-    //    Serial.print("dist_ir:");
+    //    Serial.print("ir:");
     //    Serial.print(dist_raw);
-    //    Serial.print(",dist_ema:");
+    //    Serial.print(",cal:");
+    //    Serial.print(dist_cali);
+    //    Serial.print(",m:");
+    //    Serial.print(median_pivot);
+    //    Serial.print(",ema:");
     //    Serial.print(dist_ema);
     //    Serial.print(",p:");
-    //    Serial.print(pterm * kp);
+    //    Serial.print(pterm);
     //    Serial.print(",i:");
-    //    Serial.print(iterm * ki);
+    //    Serial.print(iterm);
     //    Serial.print(",d:");
-    //    Serial.println(dterm * kd);
+    //    Serial.print(dterm);
+    //    Serial.print(",ser:");
+    //    Serial.println(duty_target);
 
     Serial.print("dist_ir:");
     Serial.print(dist_raw);
